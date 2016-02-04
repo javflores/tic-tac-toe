91b183690989f304c939a27157be178a1902e4a8
javflores <jflores@findmypast.com>
2016-02-04 08:31:00 +0000

    Adding next step where user select which player moves first. Needs further refactoring of Selection component.

--- game_ui/__tests__/selection.spec.js
+++ game_ui/__tests__/selection.spec.js
@@ -8,7 +8,7 @@ import TestUtils from 'react-addons-test-utils';
 
 const Selection = require('../components/selection');
 
-describe('Selection when rendering Tic Tac Toe', () => {
+describe('When starting TicTacToe', () => {
     let selection;
     beforeEach(() => {
         selection = TestUtils.renderIntoDocument(<Selection />);
@@ -22,13 +22,14 @@ describe('Selection when rendering Tic Tac Toe', () => {
     it('allows user to input name of first player', () => {
         let playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
 
-        playerNameInput.value = 'Juan';
+        let firstPlayerName = 'Juan';
+        playerNameInput.value = firstPlayerName;
         TestUtils.Simulate.change(playerNameInput);
 
-        expect(playerNameInput.value).toEqual("Juan");
+        expect(playerNameInput.value).toEqual(firstPlayerName);
     });
 
-    it('displays human type of player by default', () => {
+    it('displays Human-type of player by default', () => {
         let playerTypeSelect = TestUtils.findRenderedDOMComponentWithClass(selection, 'dropdown-toggle');
 
         expect(playerTypeSelect.textContent).toEqual("Human")
@@ -37,10 +38,11 @@ describe('Selection when rendering Tic Tac Toe', () => {
     it('allows user to select type of player', () => {
         let computerTypeSelect = TestUtils.findRenderedDOMComponentWithClass(selection, 'computer-type');
 
-        TestUtils.Simulate.click(computerTypeSelect, {"target": {"textContent": "Computer"}});
+        let firstPlayerType = "Computer";
+        TestUtils.Simulate.click(computerTypeSelect, {"target": {"textContent": firstPlayerType}});
 
         let playerTypeSelect = TestUtils.findRenderedDOMComponentWithClass(selection, 'dropdown-toggle');
-        expect(playerTypeSelect.textContent).toEqual("Computer")
+        expect(playerTypeSelect.textContent).toEqual(firstPlayerType)
     });
 
     it('display continue selection button if first player name is provided', () => {
@@ -52,30 +54,87 @@ describe('Selection when rendering Tic Tac Toe', () => {
         let continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'btn-primary');
         expect(continueSelection).toBeDefined();
     });
+});
 
-    it('displays second player selection when user clicks continue', () => {
-        let playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
+describe('When starting TicTacToe and first player has been selected', () => {
+    let selection,
+        playerNameInput;
+    beforeEach(() => {
+        selection = TestUtils.renderIntoDocument(<Selection />);
+        playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
         playerNameInput.value = "Juan";
         TestUtils.Simulate.change(playerNameInput);
 
-        let continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'btn-primary');
+        let continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'continue');
         TestUtils.Simulate.click(continueSelection);
-
-        expect(playerNameInput.value).toEqual("");
     });
 
-    it('display start game button if second player name is provided', () => {
-        let playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
+    it('should ask user to select second player', () => {
+        let mainText = TestUtils.findRenderedDOMComponentWithTag(selection, 'h4');
+        expect(mainText.textContent).toEqual("Select second player");
+    });
+
+    it('allows user to select player name', () => {
+        playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
+        let playerInput = playerNameInput.value;
+
+        expect(playerInput).toEqual("");
+    });
+
+    it('displays continue selection button if second player name is provided', () => {
+        playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
+        playerNameInput.value = "John";
+
+        TestUtils.Simulate.change(playerNameInput);
+
+        let continueButton = TestUtils.findRenderedDOMComponentWithClass(selection, 'btn-primary');
+        expect(continueButton).toBeDefined();
+    });
+});
+
+describe('When the two players have been provided', () => {
+    let selection;
+    beforeEach(() => {
+        selection = TestUtils.renderIntoDocument(<Selection />);
+        var playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
         playerNameInput.value = "Juan";
         TestUtils.Simulate.change(playerNameInput);
-        let continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'btn-primary');
+
+        let continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'continue');
         TestUtils.Simulate.click(continueSelection);
 
+        playerNameInput = TestUtils.findRenderedDOMComponentWithTag(selection, 'input');
         playerNameInput.value = "John";
         TestUtils.Simulate.change(playerNameInput);
 
-        let continueStartup = TestUtils.findRenderedDOMComponentWithClass(selection, 'btn-primary');
-        expect(continueStartup).toBeDefined();
+        continueSelection = TestUtils.findRenderedDOMComponentWithClass(selection, 'to-first-player');
+        TestUtils.Simulate.click(continueSelection);
+    });
+
+    it('asks user to select player that will start moving', () => {
+        let mainText = TestUtils.findRenderedDOMComponentWithTag(selection, 'h4');
+        expect(mainText.textContent).toEqual("Almost there! Select the player that will move first");
+    });
+
+    it('allows user to select player to start', () => {
+        let firstPlayerOption = TestUtils.findRenderedDOMComponentWithClass(selection, 'first-player');
+
+        let firstPlayer = "Juan";
+        let selectedPlayer = {"textContent": firstPlayer, "childNodes": [{className: "glyphicon glyphicon-user pull-right"}]};
+        TestUtils.Simulate.click(firstPlayerOption, {"target": selectedPlayer});
+
+        let playerToStartSelect = TestUtils.findRenderedDOMComponentWithClass(selection, 'dropdown-toggle');
+        expect(playerToStartSelect.textContent).toEqual(firstPlayer);
+    });
+
+    it('sets type of player icon for selected player to start', () => {
+        let firstPlayerOption = TestUtils.findRenderedDOMComponentWithClass(selection, 'first-player');
+
+        let selectedPlayer = {"textContent": "Juan", "childNodes": [{className: "glyphicon glyphicon-user pull-right"}]};
+        TestUtils.Simulate.click(firstPlayerOption, {"target": selectedPlayer});
+
+        let typeOfPlayerToStartIcon = TestUtils.scryRenderedDOMComponentsWithClass(selection, 'glyphicon glyphicon-user pull-right')[2];
+        expect(typeOfPlayerToStartIcon).toBeDefined();
     });
 
     it('should trigger initialization of the game if players were selected', () => {
--- game_ui/components/selection.js
+++ game_ui/components/selection.js
@@ -17,10 +17,12 @@ const Selection = React.createClass({
     },
 
     typeSelected(playerNumber, type){
+        let typeIcon = (type == "human") ? "glyphicon-user" : "glyphicon-blackboard";
         let players = this.state.players;
         let player = players[playerNumber-1];
 
         player.type = type;
+        player.typeIcon = typeIcon;
         this.setState({
             players: players
         });
@@ -32,32 +34,70 @@ const Selection = React.createClass({
         });
     },
 
+    continueFirstPlayerSelection(){
+        this.setState({
+            playersSelected: true
+        });
+    },
+
+    playerToStartSelected(e){
+        let playerToStartName = e.target.textContent;
+        let playerToStartIcon = e.target.childNodes[0].className;
+        let playerToStart = this.state.playerToStart;
+        playerToStart.name = playerToStartName;
+        playerToStart.typeIcon = playerToStartIcon;
+
+        this.setState({
+            playerToStart: playerToStart
+        });
+    },
+
     getInitialState(){
         return {
+            playersSelected: false,
             editingPlayerNumber: 1,
             players: [{
                 name: "",
-                type: "Human"
+                type: "Human",
+                typeIcon: "glyphicon glyphicon-user pull-right"
             },{
                 name: "",
-                type: "Human"
-            }]
+                type: "Human",
+                typeIcon: "glyphicon glyphicon-user pull-right"
+            }],
+            playerToStart: {
+                name: "",
+                typeIcon: ""
+            }
         };
     },
 
     render() {
-        if(this.state.editingPlayerNumber === 1){
+        if(this.state.playersSelected === true){
             return (
                 <div className="row">
                     <div className="row">
                         <div className="col-lg-12 title">
-                            <h4 className="game-control subheader">Select first player</h4>
+                            <h4 className="game-control subheader">Almost there! Select the player that will move first</h4>
                         </div>
                     </div>
                     <div className="row">
                         <div className="form-group">
-                            <PlayerSelection playerNumber={1} player={this.state.players[0]} nameChanged={this.nameChanged} typeSelected={this.typeSelected}/>
-                            {(this.state.players[0].name !== "") ?
+                            <div className="row">
+                                <ul className="nav navbar-nav">
+                                    <li className="dropdown">
+                                        <a className="dropdown-toggle" data-toggle="dropdown">{this.state.playerToStart.name}
+                                            <span className={this.state.playerToStart.typeIcon}/>
+                                        </a>
+                                        <ul className="dropdown-menu">
+                                            <li><a className="first-player" onClick={this.playerToStartSelected}>{this.state.players[0].name}<span className="glyphicon glyphicon-user pull-right"/></a></li>
+                                            <li className="divider"/>
+                                            <li><a className="second-player" onClick={this.playerToStartSelected}>{this.state.players[1].name}<span className="glyphicon glyphicon-user pull-right"/></a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                            {(this.state.playerToStart !== "") ?
                                 <div className="col-lg-2 continue-selection">
                                     <a className="btn btn-primary" onClick={this.continueClicked}>
                                         <i className="fa fa-arrow-right"/>
@@ -70,27 +110,53 @@ const Selection = React.createClass({
             );
         }
 
-        return (
-            <div className="row">
+        else if(this.state.editingPlayerNumber === 1){
+            return (
                 <div className="row">
-                    <div className="col-lg-12 title">
-                        <h4 className="game-control subheader">Select second player</h4>
+                    <div className="row">
+                        <div className="col-lg-12 title">
+                            <h4 className="game-control subheader">Select first player</h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group">
+                            <PlayerSelection key={1} playerNumber={1} player={this.state.players[0]} nameChanged={this.nameChanged} typeSelected={this.typeSelected}/>
+                            {(this.state.players[0].name !== "") ?
+                                <div className="col-lg-2 continue-selection">
+                                    <a className="btn btn-primary continue" onClick={this.continueClicked}>
+                                        <i className="fa fa-arrow-right"/>
+                                    </a>
+                                </div> : <div></div>
+                            }
+                        </div>
                     </div>
                 </div>
+            );
+        }
+
+        else {
+            return (
                 <div className="row">
-                    <div className="form-group">
-                        <PlayerSelection playerNumber={2} player={this.state.players[1]} nameChanged={this.nameChanged} typeSelected={this.typeSelected}/>
-                        {(this.state.players[1].name !== "") ?
-                            <div className="col-lg-2 continue-selection">
-                                <a className="btn btn-primary" onClick={this.startGame}>
-                                    <i className="fa fa-arrow-right"/>
-                                </a>
-                            </div> : <div></div>
-                        }
+                    <div className="row">
+                        <div className="col-lg-12 title">
+                            <h4 className="game-control subheader">Select second player</h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group">
+                            <PlayerSelection key={2} playerNumber={2} player={this.state.players[1]} nameChanged={this.nameChanged} typeSelected={this.typeSelected}/>
+                            {(this.state.players[1].name !== "") ?
+                                <div className="col-lg-2 continue-selection">
+                                    <a className="btn btn-primary to-first-player" onClick={this.continueFirstPlayerSelection}>
+                                        <i className="fa fa-arrow-right"/>
+                                    </a>
+                                </div> : <div></div>
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 });
 
