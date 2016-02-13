5d7d34e9436d2408bcf958a2b92fb3bd3f42bdb8
javflores <jflores@findmypast.com>
2016-02-13 11:26:01 +0000

    AttackOpponent consists on creating a chance to win: triple with two empty positions and one mark by player. I was able to use BoardCutter logic to find those kind of triples, leading to a lot of clean up of code.

--- game_engine/lib/game_engine/play_strategies/kickass_attack_opponent_moves.ex
+++ game_engine/lib/game_engine/play_strategies/kickass_attack_opponent_moves.ex
@@ -2,14 +2,15 @@ defmodule GameEngine.PlayStrategies.KickAssAttackMoves do
 	def find(board, player) do
 		board 
 		|> GameEngine.Board.two_empty_spaces_triples_in_board(player)
-		|> find_corners(player)
+		|> filter_non_duplicated
 		|> filter_possible_opponents_forks(board, player)
 		|> List.first
 	end
 
-	def find_corners(%{rows: rows, columns: columns, diagonals: diagonals}, player) do
-		find_corners_in_rows(rows, player) ++ find_corners_in_columns(columns, player) ++ find_corners_in_diagonals(diagonals, player)
-		|> Enum.uniq
+	def filter_non_duplicated(triples) do
+		%{non_duplicates: non_duplicated_spaces, all: all_two_empty_spaces} = triples
+		|> GameEngine.BoardCutter.triples_with_two_empty_spaces
+		non_duplicated_spaces
 	end
 
 	def filter_possible_opponents_forks(possible_moves, board, player) do
@@ -41,38 +42,4 @@ defmodule GameEngine.PlayStrategies.KickAssAttackMoves do
 	def know_your_enemy(:o), do: :x
 
 	def know_your_enemy(:x), do: :o
-
-	def find_corners_in_rows([], _player), do: []
-	def find_corners_in_rows({[nil, nil, mark], row}, player) when row in [0, 2] and mark == player, do: [%{row: row, column: 0}]
-	def find_corners_in_rows({[nil, mark, nil], row}, player) when row in [0, 2] and mark == player, do: [%{row: row, column: 0}, %{row: row, column: 2}]
-	def find_corners_in_rows({[mark, nil, nil], row}, player) when row in [0, 2] and mark == player, do: [%{row: row, column: 2}]
-	def find_corners_in_rows({[_, _, _], 1}, _player), do: []
-
-	def find_corners_in_rows([triple|tail], player) do
-		find_corners_in_rows(triple, player) ++ find_corners_in_rows(tail, player)
-	end
-
-	def find_corners_in_columns([], _player), do: []
-	def find_corners_in_columns({[nil, nil, mark], column}, player) when column in [0, 2] and mark == player, do: [%{row: 0, column: column}]
-	def find_corners_in_columns({[nil, mark, nil], column}, player) when column in [0, 2] and mark == player, do: [%{row: 0, column: column}, %{row: 2, column: column}]
-	def find_corners_in_columns({[mark, nil, nil], column}, player) when column in [0, 2] and mark == player, do: [%{row: 2, column: column}]
-	def find_corners_in_columns({[_, _, _], 1}, _player), do: []
-
-	def find_corners_in_columns([triple|tail], player) do
-		find_corners_in_columns(triple, player) ++ find_corners_in_columns(tail, player)
-	end
-
-	def find_corners_in_diagonals([], _player), do: []
-	def find_corners_in_diagonals({[nil, nil, mark], 0}, player) when mark == player, do: [%{row: 0, column: 0}]
-	def find_corners_in_diagonals({[nil, mark, nil], 0}, player) when mark == player, do: [%{row: 0, column: 0}, %{row: 2, column: 2}]
-	def find_corners_in_diagonals({[mark, nil, nil], 0}, player) when mark == player, do: [%{row: 2, column: 2}]
-
-	def find_corners_in_diagonals({[nil, nil, mark], 1}, player) when mark == player, do: [%{row: 0, column: 2}]
-	def find_corners_in_diagonals({[nil, mark, nil], 1}, player) when mark == player, do: [%{row: 0, column: 2}, %{row: 2, column: 0}]
-	def find_corners_in_diagonals({[mark, nil, nil], 1}, player) when mark == player, do: [%{row: 2, column: 0}]
-	def find_corners_in_diagonals({[_, _, _], _}, _player), do: []
-
-	def find_corners_in_diagonals([triple|tail], player) do
-		find_corners_in_diagonals(triple, player) ++ find_corners_in_diagonals(tail, player)
-	end
 end
--- game_engine/test/lib/kickass_strategy_attack_opponent_moves_test.exs
+++ game_engine/test/lib/kickass_strategy_attack_opponent_moves_test.exs
@@ -5,14 +5,24 @@ defmodule GameEngine.KickAssAttackMovesTest do
 	@player :o
 	@opponent :x
 
-	test "prevents fork by forcing opponent into defending" do
+	test "finds non-duplicated triples with two empty positions and one position by players" do
+		board_triples = %{rows: [{[nil, nil, @player], 0}, {[nil, nil, @player], 1}], 
+						  columns: [{[nil, nil, @player], 0}], 
+						  diagonals: [{[nil, nil, @player], 0}]}
+		non_duplicated = KickAssAttackMoves.filter_non_duplicated(board_triples)
+
+		assert non_duplicated == [%{row: 0, column: 0}, %{row: 0, column: 1}, 
+								  %{row: 1, column: 0}, %{row: 1, column: 1}]
+	end
+
+	test "atacks fork by forcing opponent into defending" do
 		positions = {@player, nil, nil,
 					 nil, @opponent, nil,
 					 nil, nil, @opponent}
 
 		move = KickAssAttackMoves.find(%GameEngine.Board{positions: positions}, @player)
 
-		assert move == %{row: 0, column: 2}
+		assert move == %{row: 0, column: 1}
 	end
 
 	test "refutes moves leading to opponent's fork in response to a force-defence move" do
@@ -45,148 +55,7 @@ defmodule GameEngine.KickAssAttackMovesTest do
 		force_fork_move = %{row: 0, column: 2}
 
 		assert KickAssAttackMoves.is_safe_defence?(force_fork_move, board, @player) == false
-	end
-
-	test "finds corners for board triples" do
-		board_triples = %{rows: [{[nil, nil, @player], 0}, {[nil, nil, @player], 1}], 
-						  columns: [{[nil, nil, @player], 0}], 
-						  diagonals: [{[nil, nil, @player], 0}]}
-		found_corners = KickAssAttackMoves.find_corners(board_triples, @player)
-
-		assert found_corners == [%{row: 0, column: 0}]
-	end
-
-	test "find top left corner in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, nil, @player], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}]
-	end
-
-	test "find top left corner when user has taken middle possition in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, @player, nil], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}, %{column: 2, row: 0}]
-	end
-
-	test "find top right corner in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[@player, nil, nil], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 2}]
-	end
-
-	test "find bottom left corner in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, nil, @player], 2}], @player)
-
-		assert found_corners == [%{row: 2, column: 0}]
-	end
-
-	test "find bottom left corner when user has taken middle possition in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, @player, nil], 2}], @player)
-
-		assert found_corners == [%{row: 2, column: 0}, %{column: 2, row: 2}]
-	end
-
-	test "cornes are not available in the middle row" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, @player, nil], 1}], @player)
-
-		assert found_corners == []
-	end
-
-	test "find bottom right corner in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[@player, nil, nil], 2}], @player)
-
-		assert found_corners == [%{row: 2, column: 2}]
-	end
-
-	test "find several corners in rows" do
-		found_corners = KickAssAttackMoves.find_corners_in_rows([{[nil, nil, @player], 0}, {[@player, nil, nil], 2}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}, %{row: 2, column: 2}]
-	end
-
-	test "find top left corner in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, nil, @player], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}]
-	end
-
-	test "find top left corner when user has taken middle possition in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, @player, nil], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}, %{column: 0, row: 2}]
-	end
-
-	test "find bottom left corner in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[@player, nil, nil], 0}], @player)
-
-		assert found_corners == [%{row: 2, column: 0}]
-	end
-
-	test "find top right corner in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, nil, @player], 2}], @player)
-
-		assert found_corners == [%{row: 0, column: 2}]
-	end
-
-	test "find bottom right corner when user has taken middle possition in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, @player, nil], 2}], @player)
-
-		assert found_corners == [%{row: 0, column: 2}, %{column: 2, row: 2}]
-	end
-
-	test "cornes are not available in the middle column" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, @player, nil], 1}], @player)
-
-		assert found_corners == []
-	end
-
-	test "find bottom right corner in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[@player, nil, nil], 2}], @player)
-
-		assert found_corners == [%{row: 2, column: 2}]
-	end
-
-	test "find several corners in columns" do
-		found_corners = KickAssAttackMoves.find_corners_in_columns([{[nil, nil, @player], 0}, {[@player, nil, nil], 2}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}, %{row: 2, column: 2}]
-	end
-
-	test "find top left corner in diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[nil, nil, @player], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}]
-	end
-
-	test "find top left corner when user has taken middle possition in diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[nil, @player, nil], 0}], @player)
-
-		assert found_corners == [%{row: 0, column: 0}, %{column: 2, row: 2}]
-	end
-
-	test "find bottom right corner in diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[@player, nil, nil], 0}], @player)
-
-		assert found_corners == [%{row: 2, column: 2}]
-	end
-
-	test "find top right corner in back diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[nil, nil, @player], 1}], @player)
-
-		assert found_corners == [%{row: 0, column: 2}]
-	end
-
-	test "find top right corner when user has taken middle possition in back diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[nil, @player, nil], 1}], @player)
-
-		assert found_corners == [%{row: 0, column: 2}, %{column: 0, row: 2}]
-	end
-
-	test "find bottom left corner in back diagonal" do
-		found_corners = KickAssAttackMoves.find_corners_in_diagonals([{[@player, nil, nil], 1}], @player)
-
-		assert found_corners == [%{row: 2, column: 0}]
-	end
+	end	
 
 	test "o finds x as opponent in order to know about his possible future movements" do
 		assert KickAssAttackMoves.know_your_enemy(@player) == @opponent
